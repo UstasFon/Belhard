b9fc1e4a4a85ca05c6164fcea5ea2c7a638d818f
UstasFon <illich1912@gmail.com>
2020-05-06 19:57:49 +0300

    start project

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './scss/index.scss';
+import './scss/vendors/_extends.scss';
+import './scss/vendors/_variables.scss';
+
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        open: true,
+        inline: true,
+        contentBase: './dist',
+        historyApiFallback: true,
+        port: 9000
+    },
+    devtool: 'source-map',
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpeg|gif|ttf|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss/,
+                use: ['style-loader', 'css-loader', { loader: 'sass-loader', options: {} }]
+            },
+            {
+                test: /\.(jpg)$/,
+                use: {
+                    loader: 'url-loader',
+                },
+            }
+        ]
+    },
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "js/bundle.js",
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ]
+};
